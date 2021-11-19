67ec43187e104f4b353175f3ba3be3dead2b755b
Andrey Galaktionov <das.frama@gmail.com>
2021-11-19 17:15:10 +0300

    Вращение планет.

--- demo/solar/solar.c
+++ demo/solar/solar.c
@@ -16,7 +16,7 @@ typedef struct {
     texture* tex;
 
     float distance;
-    float radius;
+    float diameter;
     float orb_period;
     float rot_speed;
     float tilt_angle;
@@ -30,7 +30,8 @@ static mesh* g_planet_mesh = NULL;
 static mesh_data* g_planet_md = NULL;
 static material* g_planet_mat = NULL;
 
-planet* planet_new(float distance, float radius, float orb_period, float tilt_angle, float rot_speed, const char* texture_path) {
+planet* planet_new(float distance, float diameter, float orb_period, float tilt_angle, 
+                   float rot_speed, const char* texture_path) {
     planet* p = malloc(sizeof(planet));
     memset(p, 0, sizeof(planet));
     memset(&p->cc, 0, sizeof(planet_constant));
@@ -52,9 +53,9 @@ planet* planet_new(float distance, float radius, float orb_period, float tilt_an
     renderable_set_material(p->rnd, g_planet_mat);
     p->tex = texture_load(texture_path);
 
-     // set members
+    // set members
     p->distance   = distance;
-    p->radius     = radius;
+    p->diameter   = diameter;
     p->orb_period = orb_period;
     p->tilt_angle = tilt_angle;
     p->rot_speed  = rot_speed;
@@ -62,14 +63,36 @@ planet* planet_new(float distance, float radius, float orb_period, float tilt_an
 
     p->cc.light_position = vec4_new(0, 0, 0, 0);
     p->cc.light_radius   = 400.0f;
+    // mat4 light_rot = mat4_rotate_y(-4.5f);
+    // p->cc.light_direction = vec4_new_vec3(mat4_z_direction(light_rot));
 
     return p;
 }
 
+planet* sun_new() {
+    planet* sun = planet_new(0, 20, 0, 0, 0, "assets\\textures\\sun.jpg");
+    material* sun_material = material_load("assets\\shaders\\planet_vs.hlsl", "assets\\shaders\\sun_ps.hlsl");
+    renderable_set_material(sun->rnd, sun_material);
+
+    return sun;
+}
+
+
 void planet_delete(planet* p) {
-	texture_unload(p->tex);
+    texture_unload(p->tex);
     free(p->rnd);
     free(p);
+}
+
+void sun_delete(planet* sun) {
+    material_unload(sun->rnd->material);
+    planet_delete(sun);
+}
+
+
+void planet_delete_static() {
+    mesh_delete(g_planet_mesh);
+    material_unload(g_planet_mat);
 }
 
 void planet_update(planet* p, float dt) {
@@ -77,11 +100,11 @@ void planet_update(planet* p, float dt) {
     p->pos.y = 0;
     p->pos.z = 0.8f * p->distance * cos(p->orb_period*dt);
 
-    p->rot_angle += p->rot_speed * 0.05f;
+    p->rot_angle += p->rot_speed;
 }
 
 void planet_draw(planet* p, const camera* cam) {
-    mat4 scale = mat4_scale_by(p->radius);
+    mat4 scale = mat4_scale_by(p->diameter);
     mat4 rotate = mat4_rotate_y(to_rad(p->rot_angle));
     mat4 translate = mat4_translate(p->pos);
     mat4 world = mat4_mul_x(3, scale, rotate, translate);
@@ -91,15 +114,13 @@ void planet_draw(planet* p, const camera* cam) {
     p->cc.proj  = cam->proj;
     p->cc.camera_position = vec4_new_vec3(mat4_translation(cam->world));
 
-    mat4 light_rot = mat4_rotate_y(-4.5f);
-    p->cc.light_direction = vec4_new_vec3(mat4_z_direction(light_rot))  ;
-
     material_set_data(p->rnd->material, &p->cc, sizeof(planet_constant));
     material_replace_texture(p->rnd->material, 0, p->tex);
 
     graphics_draw(p->rnd);
 }
 
+#define PLANET_COUNT 5
 int main() {
 	// init engine.
 	motor_init("D3D11 Solar System", 1920, 1080);
@@ -110,7 +131,18 @@ int main() {
 	grid* grid = grid_new(1024, 1024, 64, 64, vec4_new(.5f, .5f, .5f, .25f));
 
 	// Planet.
-	planet* sun = planet_new(0, 20.0f, 2.0f, 0, 0, "assets\\textures\\sun.jpg");
+    planet* planets[PLANET_COUNT];
+    // distance, diameter,  orb_period, tilt_angle, rot_speed
+    // sun
+	planets[0] = sun_new();
+    // mercury
+    planets[1] = planet_new(30, 1, 1, 0.0f, 0.0171f, "assets\\textures\\mercury.jpg");
+    // venus
+    planets[2] = planet_new(42, 2.85f, 0.39f, -177.4f, 0.0041f, "assets\\textures\\venus.jpg");
+    // earth
+    planets[3] = planet_new(55, 3, 0.24f, -23.0f, 1, "assets\\textures\\earth_day.jpg");
+    // mars
+    planets[4] = planet_new(68, 1.5f, 0.13f, -25.2f, 0.9756f, "assets\\textures\\mars.jpg");
 
 	// Input.
 	input in = {0};
@@ -131,23 +163,27 @@ int main() {
 		// Update.
 		camera_update(cam, &in, frame_dt());
 		grid_update(grid, cam);
-		planet_update(sun, planet_time);
+        for (int i = 0; i < PLANET_COUNT; i++) {
+		  planet_update(planets[i], planet_time);
+        }
 		planet_time += 0.005f;
 
 		// Render.
 		graphics_clear_screen(0.0f, 0.0f, 0.0f, 0.0f);
 		grid_draw(grid);
-		planet_draw(sun, cam);
+		for (int i = 0; i < PLANET_COUNT; i++) {
+          planet_draw(planets[i], cam);
+        }
 		graphics_present();
 
 		frame_end();
 	}
 
 	// Release game resources.
-	// mesh_unload(planet_mesh);
-	// mesh_unload(skybox_mesh);
-	planet_delete(sun);
-	// light_delete(light);
+    sun_delete(planets[0]);
+	for (int i = 1; i < PLANET_COUNT; i++) {
+        planet_delete(planets[i]);
+    }
 
 	// Release engine resources.
 	grid_delete(grid);
